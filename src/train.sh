e894c2695720577fa2cadf36831ab6079460df3f
Bianca-Ştefania MUŞAT (94612) <bianca.musat@stud.acs.upb.ro>
2023-03-12 17:47:30 +0200

    Modified config_bert_sep to run for 10 epochs and modified train.sh with test dataset instead of dev dataset

--- src/train.sh
+++ src/train.sh
@@ -3,6 +3,7 @@
 
 train_data=../data/VAST/vast_train.csv
 dev_data=../data/VAST/vast_dev.csv
+test_data=../data/VAST/vast_test.csv
 
 echo "training model with early stopping and $3 warm-up epochs"
-python train_model.py -s $1 -i ${train_data} -d ${dev_data} -e 1 -p $2 -k $3
+python train_model.py -s $1 -i ${train_data} -d ${test_data} -e 1 -p $2 -k $3
